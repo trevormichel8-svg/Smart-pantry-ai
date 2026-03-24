f0753c0a8c435623e12e7ead4a3a70f7fa5fae6d
trevormichel8-svg <trevormichel8@gmail.com>
2026-03-23 23:00:59 -0600

    Use Clerk recommended middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
